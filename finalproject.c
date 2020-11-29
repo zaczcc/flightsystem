42c3c270703676cd7884249389ed3b74ae5213fd
addison110 <58442231+addison110@users.noreply.github.com>
2020-11-29 18:53:00 -0500

    Add files via upload

---
+++ finalproject.c
@@ -0,0 +1,12 @@
+/*
+Title: Flight Management system
+Desc: This program contains information about flights and lets users book flights
+Name: Addison Wenzel, Zac Chen, Andy Lin, Chris DeMuyt, Evan Michael Harding
+Email: wenzela@purdue.edu, lin1019@purdue.edu, chen2946@purdue.edu, cdemuyt@purdue.edu, hardine@purdue.edu
+*/
+
+#include <stdio.h>
+
+void main(){
+        printf("\ntest\n");
+}
